49e20d17dae572be4711a1e9d11a3ee49e5ea847
vlw <vlwwwwww@gmail.com>
2016-10-07 19:36:00 +0300

    Update device
    Change-Id: I15ee334e7f82d7e9b02e2486067b51b80d5612c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,10 @@
 TARGET_OTA_ASSERT_DEVICE := a3ltexx,a3ultexx,a33gxx,a33g
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogen_a3ltexx_defconfig
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig 
+#cyanogen_a3ltexx_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a3_eur_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_NO_RECOVERY := true
 
 # TWRP
