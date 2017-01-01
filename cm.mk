3823949b9fd7042474794dee3484bc4c212e34bc
vlw <vlwwwwww@gmail.com>
2017-01-01 14:39:44 +0300

    remove nfc-e for 14.1
    Change-Id: I861338825914e1acef8dc29838df30de9bb53f05

--- cm.mk
+++ cm.mk
@@ -1,9 +1,6 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/a3ltexx/full_a3ltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=a3ltexx TARGET_DEVICE=a3lte
