bfeaa9040a475836bc2dda4c04e32554f78c718f
vlw <vlwwwwww@gmail.com>
2017-07-23 14:50:36 +0300

    a3lte: Add OTA assert list
    Change-Id: Idcda4a864dc3c98bc1bcce976fb07d6c30894e66

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,8 @@
 # Inherit from the proprietary version
 -include vendor/samsung/a3lte/BoardConfigVendor.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := a3ltexx,a3ultexx,a33gxx,a33g,a3lte,a3ulte
+
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a3_eur_defconfig
