c437677ae3af3a5e6399538d42916fa502fbf3c7
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-09-01 14:21:55 -0400

    a3lte: Clean up ota device assertion list
    Change-Id: Icb277360384de4db82b83fc6fc089de4b77b0fb1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 -include device/samsung/a3-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := a3ltexx,a3ultexx,a33gxx,a33g,a3lte,a3ulte
+TARGET_OTA_ASSERT_DEVICE := a3lte,a3ltexx
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a3_eur_defconfig
