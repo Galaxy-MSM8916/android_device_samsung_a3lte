fec031aa42e0e26455ed332e931180f401621afd
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-02 13:22:11 -0400

    a3lte: Enable zram
     - Enable for 1 GB devices
    
    Change-Id: Id21e4d6123243fcd66a6401e2a5e7e9f2c55d46a

--- system_prop.mk
+++ system_prop.mk
@@ -5,7 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RAM
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false \
-	ro.config.zram=false
+	ro.config.zram.enabled=true
 
 # Screen
 PRODUCT_PROPERTY_OVERRIDES += \
