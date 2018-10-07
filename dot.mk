2278d79eb31321e0d4d217465ac8b35dc81db522
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:49 -0400

    a3lte: Add dotOS Makefile
    Change-Id: I603fc4eae5a9964b63a5fc0cd426cbef482b5899

---
+++ dot.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017 The Dot OS project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Inherit from common
+$(call inherit-product, device/samsung/a3-common/dot.mk)
+
+$(call inherit-product, device/samsung/a3lte/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a3lte
+PRODUCT_NAME := dot_a3lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A300F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
